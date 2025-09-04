f8a687703bb66a3809ff46e65e28569609923977
Ylese <ylese.jose@gmail.com>
2025-09-04 15:34:41 +1200

    first commit

---
+++ Main.py
@@ -0,0 +1,4 @@
+import MathFunctions
+
+result = MathFunctions.add(5, 7)
+print("5 + 7 =", result)
---
+++ MathFunctions.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+print("This line runs no matter what")
+
+if __name__ == "__main__":
+    print("2 + 3 =", add(2, 3))
